0f77651c9c62d84e9d2955165ca25c1f1fc91756
Perdana Hadi Sanjaya <ceroberoz@gmail.com>
2017-07-18 15:23:26 +0700

    magento training day 2

---
+++ app/code/local/Brandoutlet/Training/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+Class Brandoultlet_Training_Helper_Data extends Mage_Core_Helper_Abstract()
+{
+  public function showHello()
+  {
+    echo "Hello Me!";
+  }
+}
+
+?>
---
+++ app/code/local/Brandoutlet/Training/controller/indexController.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Brandoutlet_Training_IndexController extends Mage_Core_Controller_Front_Action
+{
+  public function indexAction()
+  {
+    echo "meong";
+  }
+}
+
+?>
